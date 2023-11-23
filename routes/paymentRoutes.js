de99dbdc9bf37a773759b7ecda621545f9956ec9
Yunus Mustapha <yunusmustapha2.com>
2023-11-23 16:06:50 +0100

    disable payment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,59 +1,59 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
+// const express = require('express');
+// const router = express.Router();
+// const axios = require('axios');
 
-const mySecretKey = process.env.PAYSTACK_KEY; 
+// const mySecretKey = process.env.PAYSTACK_KEY; 
 
-router.post('/initialize-payment', async (req, res) => {
-  try {
-    const { amount, email } = req.body;
+// router.post('/initialize-payment', async (req, res) => {
+//   try {
+//     const { amount, email } = req.body;
 
-    const response = await axios.post(
-      'https://api.paystack.co/transaction/initialize',
-      {
-        email,
-        amount: amount * 100, 
-      },
-      {
+//     const response = await axios.post(
+//       'https://api.paystack.co/transaction/initialize',
+//       {
+//         email,
+//         amount: amount * 100, 
+//       },
+//       {
 
-        method: 'POST',
-        headers: {
-          Authorization: mySecretKey,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+//         method: 'POST',
+//         headers: {
+//           Authorization: mySecretKey,
+//           'Content-Type': 'application/json',
+//         },
+//       }
+//     );
 
-    const { data } = response;
+//     const { data } = response;
 
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
-router.post('/verify-payment', async (req, res) => {
-  try {
-    const { reference } = req.body;
+// router.post('/verify-payment', async (req, res) => {
+//   try {
+//     const { reference } = req.body;
 
-    const response = await axios.get(
-      `https://api.paystack.co/transaction/verify/${reference}`,
-      {
-        headers: {
-          Authorization: mySecretKey,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+//     const response = await axios.get(
+//       `https://api.paystack.co/transaction/verify/${reference}`,
+//       {
+//         headers: {
+//           Authorization: mySecretKey,
+//           'Content-Type': 'application/json',
+//         },
+//       }
+//     );
 
-    const { data } = response;
+//     const { data } = response;
 
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
-module.exports = {router};
+// module.exports = {router};

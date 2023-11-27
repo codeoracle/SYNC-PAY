0cf4a1f9726ae8054b54ffea8f49593a7bb4d5d5
Yunus Mustapha <yunusmustapha2.com>
2023-11-27 19:45:57 +0100

    bizznm

--- routes/businessOwner.js
+++ routes/businessOwner.js
@@ -2,8 +2,8 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
-const Product = require('../models/Product');
-const Invoice = require('../models/Invoice');
+// const Product = require('../models/Product');
+const User = require('../models/User');
 const Client = require('../models/Client');
 const { authenticateToken, authorizeRole } = require('../middleware/authMiddleware');
 
@@ -42,7 +42,6 @@ router.post('/create-client', authenticateToken, authorizeRole('businessOwner'),
     res.status(500).json({ error: 'Internal Server Error', success: false });
   }
 });
-
 // Client login
 router.post('/client-login', async (req, res) => {
   try {
@@ -60,6 +59,9 @@ router.post('/client-login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials', success: false });
     }
 
+    // Fetch the business owner's information based on the businessOwnerId
+    const businessOwner = await User.findById(client.businessOwnerId);
+
     // Generate JWT token
     const token = jwt.sign(
       { email: client.email, role: 'client' },
@@ -67,12 +69,57 @@ router.post('/client-login', async (req, res) => {
       { expiresIn: '3d' }
     );
 
-    res.status(200).json({ client, token, success: true });
+    // Include business owner's information in the response
+    res.status(200).json({
+      client: {
+        _id: client._id,
+        firstName: client.firstName,
+        lastName: client.lastName,
+        email: client.email,
+        businessOwner: {
+          _id: businessOwner._id,
+          name: businessOwner.businessName, // Adjust the property accordingly
+          email: businessOwner.email,
+        },
+      },
+      token,
+      success: true,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error', success: false });
   }
 });
+
+// router.post('/client-login', async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if client exists
+//     const client = await Client.findOne({ email });
+//     if (!client) {
+//       return res.status(401).json({ error: 'Invalid credentials', success: false });
+//     }
+
+//     // Verify the password
+//     const passwordMatch = await bcrypt.compare(password, client.password);
+//     if (!passwordMatch) {
+//       return res.status(401).json({ error: 'Invalid credentials', success: false });
+//     }
+
+//     // Generate JWT token
+//     const token = jwt.sign(
+//       { email: client.email, role: 'client' },
+//       process.env.JWT_SECRET,
+//       { expiresIn: '3d' }
+//     );
+
+//     res.status(200).json({ client, token, success: true });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error', success: false });
+//   }
+// });
 
 // Create a new product and generate an invoice
 // router.post('/create-product', authenticateToken, authorizeRole('businessOwner'), async (req, res) => {
